b97e744404f0d73de94c1e591a06699ea7b12253
Daniel Ontiveros <cardanont@gmail.com>
2019-07-12 11:37:11 -0500

    Implemented Array on JS

---
+++ MyArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class MyArray
+    {
+        MyArray(int length, object[] data)
+        {
+            length = 0;
+        }
+    }
+}
